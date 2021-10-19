9d43aa1f024a5fe11eef6fc17828d99fee713bf6
EdgarAraya <44482655+EdgarAraya@users.noreply.github.com>
2021-10-19 19:06:11 -0300

    test form

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -3,11 +3,12 @@ import { NgModule } from '@angular/core';
 import { MatButtonModule} from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import {MatListModule} from '@angular/material/list';
-
+import {MatFormFieldModule} from '@angular/material/form-field'
 const MaterialComponents = [
   MatButtonModule,
   MatCardModule,
-  MatListModule
+  MatListModule,
+  MatFormFieldModule
 ]
 
 
