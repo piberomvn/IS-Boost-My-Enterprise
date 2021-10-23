65390705d730715a6ddd3eb99998338084de9f06
EdgarAraya <44482655+EdgarAraya@users.noreply.github.com>
2021-10-23 18:22:18 -0300

    form-control

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -3,16 +3,19 @@ import { NgModule } from '@angular/core';
 import { MatButtonModule} from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import {MatListModule} from '@angular/material/list';
-import {MatFormFieldModule} from '@angular/material/form-field'
-import {MatToolbarModule} from '@angular/material/toolbar'
-import {MatIconModule} from '@angular/material/icon';;
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatInputModule} from '@angular/material/input';
+
 const material = [
   MatButtonModule,
   MatCardModule,
   MatListModule,
   MatFormFieldModule,
   MatToolbarModule,
-  MatIconModule
+  MatIconModule,
+  MatInputModule
 ]
 
 
